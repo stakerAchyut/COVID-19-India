874b13869a99650ba0ff2e470ce6f4ec9c334c8f
Achyut Utpal <achyut.utpal@highradius.com>
2020-05-03 18:38:32 +0530

    build file

---
+++ build/precache-manifest.788f4c60e0688af6398d833c5b34ad8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4230ac43969101744c9568c587b6eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e108bab2e91d6bf71cc",
+    "url": "/static/css/main.39e6e93a.chunk.css"
+  },
+  {
+    "revision": "94474027cc81110e67e6",
+    "url": "/static/js/2.42b6382a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.42b6382a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e108bab2e91d6bf71cc",
+    "url": "/static/js/main.e0a0aaf3.chunk.js"
+  },
+  {
+    "revision": "8f042c284597415f8869",
+    "url": "/static/js/runtime-main.c8325658.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid.d7265326.png"
+  }
+]);
